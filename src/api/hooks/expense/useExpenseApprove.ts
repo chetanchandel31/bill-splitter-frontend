bbeedc792e1e2ca98f41d254a52f3ffadfad9971
chetan chandel <chetanchandel3@gmail.com>
2022-10-16 00:47:58 +0530

    expense-approve and expense-settle hooks

---
+++ src/api/hooks/expense/useExpenseApprove.ts
@@ -0,0 +1,45 @@
+import {
+  MutationFunction,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { API } from "api/API";
+import { AxiosErrorBillSplitter } from "types";
+
+export type SuccessResponseExpenseApprove = { ok: boolean };
+
+type PayloadExpenseApprove = {
+  groupId: string;
+  expenseId: string;
+  borrowerId: string;
+};
+
+type UseExpenseApproveParams = {
+  onError?: (error: AxiosErrorBillSplitter) => void;
+  onSuccess?: (data: SuccessResponseExpenseApprove) => void;
+};
+
+const expenseApprove: MutationFunction<
+  SuccessResponseExpenseApprove,
+  PayloadExpenseApprove
+> = ({ borrowerId, expenseId }) => {
+  return API.post(`/expenses/${expenseId}/borrowers/${borrowerId}/approve`);
+};
+
+const useExpenseApprove = ({ onError, onSuccess }: UseExpenseApproveParams) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    SuccessResponseExpenseApprove,
+    AxiosErrorBillSplitter,
+    PayloadExpenseApprove
+  >(expenseApprove, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["group"]);
+      if (typeof onSuccess === "function") onSuccess(data);
+    },
+    onError,
+  });
+};
+
+export default useExpenseApprove;
---
+++ src/api/hooks/expense/useExpenseSettle.ts
@@ -0,0 +1,44 @@
+import {
+  MutationFunction,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import { API } from "api/API";
+import { AxiosErrorBillSplitter } from "types";
+
+export type SuccessResponseExpenseSettle = { ok: boolean };
+
+type PayloadExpenseSettle = {
+  groupId: string;
+  expenseId: string;
+};
+
+type UseExpenseSettleParams = {
+  onError?: (error: AxiosErrorBillSplitter) => void;
+  onSuccess?: (data: SuccessResponseExpenseSettle) => void;
+};
+
+const expenseSettle: MutationFunction<
+  SuccessResponseExpenseSettle,
+  PayloadExpenseSettle
+> = ({ expenseId }) => {
+  return API.post(`/expenses/${expenseId}/settle`);
+};
+
+const useExpenseSettle = ({ onError, onSuccess }: UseExpenseSettleParams) => {
+  const queryClient = useQueryClient();
+
+  return useMutation<
+    SuccessResponseExpenseSettle,
+    AxiosErrorBillSplitter,
+    PayloadExpenseSettle
+  >(expenseSettle, {
+    onSuccess: (data) => {
+      queryClient.invalidateQueries(["group"]);
+      if (typeof onSuccess === "function") onSuccess(data);
+    },
+    onError,
+  });
+};
+
+export default useExpenseSettle;
