8b5e838d5b358133f82ac37a327a18fac96e346c
chetan chandel <chetanchandel3@gmail.com>
2022-10-16 23:39:05 +0530

    feat: integrate expense settle endpoint

--- src/components/expenses-personal/TabAmountToReturn.tsx
+++ src/components/expenses-personal/TabAmountToReturn.tsx
@@ -1,13 +1,23 @@
 import { CheckOutlined, ExclamationCircleOutlined } from "@ant-design/icons";
-import { Button, Modal, Skeleton, Space, Spin, Table, Typography } from "antd";
+import {
+  Button,
+  message,
+  Modal,
+  Skeleton,
+  Space,
+  Spin,
+  Table,
+  Typography,
+} from "antd";
 import type { ColumnsType } from "antd/es/table";
 import Tooltip from "antd/es/tooltip";
+import useExpenseSettle from "api/hooks/expense/useExpenseSettle";
 import { useAuth } from "contexts/auth-context";
 import { useSelectedGroup } from "contexts/group-context";
 import moment from "moment";
 import { ReactNode } from "react";
 import { Borrower } from "types";
-import { getParticipantDetails } from "utils";
+import { getParticipantDetails, showErrorMessage } from "utils";
 import { getFormattedCurrencyString } from "utils/getFormattedCurrencyString";
 import AmountToReturnStatus from "./helpers/AmountToReturnStatus";
 
@@ -15,92 +25,117 @@ import AmountToReturnStatus from "./helpers/AmountToReturnStatus";
 type DataType = {
   key: string;
   expense: string;
+  expenseId: string;
   lender: string;
   amount: number;
   recordedAt: number;
   borrower: Borrower;
 };
 
-const handleConfirm = (record: DataType) => {
-  Modal.confirm({
-    title: "Are you sure?",
-    icon: <ExclamationCircleOutlined />,
-    content: (
-      <>
-        You are claiming that you have returned{" "}
-        <strong>{getFormattedCurrencyString({ amount: record.amount })}</strong>{" "}
-        back to <strong>{record.lender}</strong> for the expense{" "}
-        <strong>{record.expense}</strong> ?
-      </>
-    ),
-    okText: "Yes",
-    cancelText: "No",
-    onOk: () => {
-      console.log("fire api call 🚀");
-    },
-  });
-};
-
-const columns: ColumnsType<DataType> = [
-  {
-    title: "Expense",
-    dataIndex: "expense",
-    key: "expense",
-    render: (text, record) => (
-      <>
-        <div>{text}</div>
-        <Typography.Text type="secondary">
-          <small>{moment.unix(record.recordedAt / 1000).format("lll")}</small>{" "}
-        </Typography.Text>
-      </>
-    ),
-  },
-  {
-    title: "Lender",
-    dataIndex: "lender",
-    key: "lender",
-    render: (text) => <>{text}</>,
-  },
-  {
-    title: "Amount Borrowed",
-    dataIndex: "amount",
-    key: "amount",
-    render: (text, record) => (
-      <>{getFormattedCurrencyString({ amount: record.amount })}</>
-    ),
-  },
-  {
-    title: "Status",
-    key: "status",
-    render: (_, record) => <AmountToReturnStatus borrower={record.borrower} />,
-  },
-  {
-    title: "",
-    key: "action",
-    render: (_, record) => (
-      <Space size="middle">
-        <Tooltip title="Claim that you have paid this amount back.">
-          <Button
-            icon={<CheckOutlined />}
-            onClick={() => handleConfirm(record)}
-            shape="circle"
-            size="small"
-            type="primary"
-          />
-        </Tooltip>
-      </Space>
-    ),
-  },
-];
-
 const TabAmountToReturn = () => {
   const { userInfo } = useAuth();
-
   const {
     isSelectedGroupFetching,
     isSelectedGroupLoading,
     selectedGroupDetails,
   } = useSelectedGroup();
+
+  const { isLoading: isExpenseSettleLoading, mutate: settleExpense } =
+    useExpenseSettle({
+      onError: showErrorMessage,
+      onSuccess: () => message.success("Expense claimed as settled"),
+    });
+
+  const handleConfirm = (record: DataType) => {
+    Modal.confirm({
+      title: "Are you sure?",
+      icon: <ExclamationCircleOutlined />,
+      content: (
+        <>
+          You are claiming that you have returned{" "}
+          <strong>
+            {getFormattedCurrencyString({ amount: record.amount })}
+          </strong>{" "}
+          back to <strong>{record.lender}</strong> for the expense{" "}
+          <strong>{record.expense}</strong> ?
+        </>
+      ),
+      okText: "Yes",
+      cancelText: "No",
+      onOk: () => {
+        const expenseId = record.expenseId;
+        const groupId = selectedGroupDetails?._id;
+
+        if (groupId) {
+          settleExpense({ expenseId, groupId });
+        }
+      },
+    });
+  };
+
+  const columns: ColumnsType<DataType> = [
+    {
+      title: "Expense",
+      dataIndex: "expense",
+      key: "expense",
+      render: (text, record) => (
+        <>
+          <div>{text}</div>
+          <Typography.Text type="secondary">
+            <small>{moment.unix(record.recordedAt / 1000).format("lll")}</small>{" "}
+          </Typography.Text>
+        </>
+      ),
+    },
+    {
+      title: "Lender",
+      dataIndex: "lender",
+      key: "lender",
+      render: (text) => <>{text}</>,
+    },
+    {
+      title: "Amount Borrowed",
+      dataIndex: "amount",
+      key: "amount",
+      render: (text, record) => (
+        <>{getFormattedCurrencyString({ amount: record.amount })}</>
+      ),
+    },
+    {
+      title: "Status",
+      key: "status",
+      render: (_, record) => (
+        <AmountToReturnStatus borrower={record.borrower} />
+      ),
+    },
+    {
+      title: "",
+      key: "action",
+      render: (_, record) => (
+        <Space size="middle">
+          <Tooltip
+            title={
+              record.borrower.isSettled || record.borrower.isApprovedByLender
+                ? ""
+                : "Claim that you have paid this amount back."
+            }
+          >
+            <Button
+              disabled={
+                record.borrower.isSettled || record.borrower.isApprovedByLender
+              }
+              icon={<CheckOutlined />}
+              loading={isExpenseSettleLoading}
+              onClick={() => handleConfirm(record)}
+              shape="circle"
+              size="small"
+              type="primary"
+            />
+          </Tooltip>
+        </Space>
+      ),
+    },
+  ];
 
   // filter out from total expenses array
   const tableData: DataType[] = [];
@@ -122,6 +157,7 @@ const TabAmountToReturn = () => {
         tableData.push({
           amount,
           expense: expense.expenseTitle,
+          expenseId: expense._id,
           key: borrower._id,
           lender,
           recordedAt: expense.recordedAt,
