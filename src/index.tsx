2e192fc467da29529b665a6dedfc627adcffcd4f
chetan chandel <chetanchandel3@gmail.com>
2022-10-23 13:53:54 +0530

    refactor: add test utils and move routes to separate dir

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRoutes from "./AppRoutes";
+import AppRoutes from "./routes/AppRoutes";
 import "antd/dist/antd.min.css";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
