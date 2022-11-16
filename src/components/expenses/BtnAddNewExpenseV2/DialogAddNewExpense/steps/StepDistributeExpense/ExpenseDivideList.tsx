7a18ed96312a4db163127bddecea8ddeec3b283f
chetan chandel <chetanchandel3@gmail.com>
2022-11-17 00:51:26 +0530

    refactor: seperate out ExpenseDivideList component

---
+++ src/components/expenses/BtnAddNewExpenseV2/DialogAddNewExpense/steps/StepDistributeExpense/ExpenseDivideList.tsx
@@ -0,0 +1,134 @@
+import { Divider, InputNumber, Tag } from "antd";
+import { actionTypeNewExpenseMeta } from "components/expenses/BtnAddNewExpenseV2/state/actions";
+import { NewExpenseMeta } from "components/expenses/BtnAddNewExpenseV2/state/reducers";
+import { useAuth } from "contexts/auth-context";
+import { useSelectedGroup } from "contexts/group-context";
+import { Dispatch } from "react";
+import { getParticipantDetails } from "utils";
+import { getFormattedCurrencyString } from "utils/getFormattedCurrencyString";
+import styles from "./stepDistributeExpense.module.css";
+
+type PropsExpenseDivideList = {
+  newExpenseMeta: NewExpenseMeta;
+  dispatch: Dispatch<actionTypeNewExpenseMeta>;
+};
+
+const ExpenseDivideList = ({
+  dispatch,
+  newExpenseMeta,
+}: PropsExpenseDivideList) => {
+  const { userInfo } = useAuth();
+  const { selectedGroupDetails } = useSelectedGroup();
+
+  const borrowerExpenseTotal: number = Object.keys(
+    newExpenseMeta.distributedTotalExpense.borrowerToExpenseMap
+  ).reduce(
+    (prev, current) =>
+      prev +
+      newExpenseMeta.distributedTotalExpense.borrowerToExpenseMap[current],
+    0
+  );
+
+  const onOwnExpenseShareChange = (amount: number) => {
+    dispatch({
+      type: "UPDATE_OWN_SHARE_IN_EXPENSE_DISTRIBUTION",
+      payload: { amount },
+    });
+  };
+
+  const onParticipantExpenseChange = (
+    amount: number,
+    participantId: string
+  ) => {
+    dispatch({
+      type: "UPDATE_PARTICIPANT_EXPENSE_DISTRIBUTION",
+      payload: {
+        amount,
+        participantId,
+      },
+    });
+  };
+
+  const borrowersList: JSX.Element[] = [];
+
+  newExpenseMeta.arrayOfSelectedParticipantId.forEach((participantId) => {
+    const participant = getParticipantDetails({
+      participantId,
+      selectedGroupDetails,
+    });
+
+    borrowersList.push(
+      <div className={styles.expenseDivideListItem} key={participant?._id}>
+        <div className={styles.expenseDivideListItemLeft}>
+          <strong>{participant?.name}</strong> ({participant?.email})
+          <div>
+            <Tag color="warning" style={{ userSelect: "none" }}>
+              Borrower
+            </Tag>
+          </div>
+        </div>
+
+        <div className={styles.expenseDivideListItemRight}>
+          <InputNumber
+            type="number"
+            onChange={(value) => {
+              onParticipantExpenseChange(value, participantId);
+            }}
+            value={
+              newExpenseMeta.distributedTotalExpense.borrowerToExpenseMap[
+                participantId
+              ]
+            }
+          />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className={styles.expenseDivideListItem}>
+        <div className={styles.expenseDivideListItemLeft}>
+          <strong>{userInfo?.user.name}</strong> ({userInfo?.user.email}){" "}
+          <div>
+            <Tag color="success" style={{ userSelect: "none" }}>
+              Lender
+            </Tag>
+            <Tag style={{ userSelect: "none" }}>(You)</Tag>
+          </div>
+        </div>
+
+        <div className={styles.expenseDivideListItemRight}>
+          <InputNumber
+            onChange={onOwnExpenseShareChange}
+            value={
+              newExpenseMeta.distributedTotalExpense.amountPaidForOwnExpense
+            }
+          />
+        </div>
+      </div>
+
+      <div>{borrowersList}</div>
+
+      <Divider />
+
+      <div className={styles.expenseDivideListItem}>
+        <div className={styles.expenseDivideListItemLeft}>
+          <strong>Total:</strong>
+        </div>
+
+        <div className={styles.expenseDivideListItemRight}>
+          <strong>
+            {getFormattedCurrencyString({
+              amount:
+                borrowerExpenseTotal +
+                newExpenseMeta.distributedTotalExpense.amountPaidForOwnExpense,
+            })}
+          </strong>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ExpenseDivideList;
--- src/components/expenses/BtnAddNewExpenseV2/DialogAddNewExpense/steps/StepDistributeExpense/index.tsx
+++ src/components/expenses/BtnAddNewExpenseV2/DialogAddNewExpense/steps/StepDistributeExpense/index.tsx
@@ -1,12 +1,9 @@
-import { Button, Divider, Input, InputNumber, Tag, Typography } from "antd";
+import { Button, Input, Typography } from "antd";
 import { actionTypeNewExpenseMeta } from "components/expenses/BtnAddNewExpenseV2/state/actions";
 import { NewExpenseMeta } from "components/expenses/BtnAddNewExpenseV2/state/reducers";
-import { useAuth } from "contexts/auth-context";
-import { useSelectedGroup } from "contexts/group-context";
 import { ChangeEvent, Dispatch } from "react";
-import { getParticipantDetails } from "utils";
-import { getFormattedCurrencyString } from "utils/getFormattedCurrencyString";
 import isExpenseDistributionInitialised from "../utils/isExpenseDistributionInitialised";
+import ExpenseDivideList from "./ExpenseDivideList";
 import styles from "./stepDistributeExpense.module.css";
 
 type PropsStepExpenseDetails = {
@@ -18,42 +15,10 @@ const StepDistributeExpense = ({
   dispatch,
   newExpenseMeta,
 }: PropsStepExpenseDetails) => {
-  const { userInfo } = useAuth();
-  const { selectedGroupDetails } = useSelectedGroup();
-
-  const borrowerExpenseTotal: number = Object.keys(
-    newExpenseMeta.distributedTotalExpense.borrowerToExpenseMap
-  ).reduce(
-    (prev, current) =>
-      prev +
-      newExpenseMeta.distributedTotalExpense.borrowerToExpenseMap[current],
-    0
-  );
-
   const onExpenseTotalChange = (e: ChangeEvent<HTMLInputElement>) => {
     dispatch({
       type: "SET_TOTAL_EXPENSE_AMOUNT",
       payload: Number(e.target.value),
-    });
-  };
-
-  const onOwnExpenseShareChange = (amount: number) => {
-    dispatch({
-      type: "UPDATE_OWN_SHARE_IN_EXPENSE_DISTRIBUTION",
-      payload: { amount },
-    });
-  };
-
-  const onParticipantExpenseChange = (
-    amount: number,
-    participantId: string
-  ) => {
-    dispatch({
-      type: "UPDATE_PARTICIPANT_EXPENSE_DISTRIBUTION",
-      payload: {
-        amount,
-        participantId,
-      },
     });
   };
 
@@ -81,90 +46,10 @@ const StepDistributeExpense = ({
 
         <div className={styles.expenseDivideListContainer}>
           {isExpenseDistributionInitialised(newExpenseMeta) ? (
-            <>
-              <div className={styles.expenseDivideListItem}>
-                <div className={styles.expenseDivideListItemLeft}>
-                  <strong>{userInfo?.user.name}</strong> ({userInfo?.user.email}
-                  ){" "}
-                  <div>
-                    <Tag color="success" style={{ userSelect: "none" }}>
-                      Lender
-                    </Tag>
-                    <Tag style={{ userSelect: "none" }}>(You)</Tag>
-                  </div>
-                </div>
-
-                <div className={styles.expenseDivideListItemRight}>
-                  <InputNumber
-                    onChange={onOwnExpenseShareChange}
-                    value={
-                      newExpenseMeta.distributedTotalExpense
-                        .amountPaidForOwnExpense
-                    }
-                  />
-                </div>
-              </div>
-
-              <div>
-                {newExpenseMeta.arrayOfSelectedParticipantId.map(
-                  (participantId) => {
-                    const participant = getParticipantDetails({
-                      participantId,
-                      selectedGroupDetails,
-                    });
-
-                    return (
-                      <div
-                        className={styles.expenseDivideListItem}
-                        key={participant?._id}
-                      >
-                        <div className={styles.expenseDivideListItemLeft}>
-                          <strong>{participant?.name}</strong> (
-                          {participant?.email})
-                          <div>
-                            <Tag color="warning" style={{ userSelect: "none" }}>
-                              Borrower
-                            </Tag>
-                          </div>
-                        </div>
-
-                        <div className={styles.expenseDivideListItemRight}>
-                          <InputNumber
-                            type="number"
-                            onChange={(value) => {
-                              onParticipantExpenseChange(value, participantId);
-                            }}
-                            value={
-                              newExpenseMeta.distributedTotalExpense
-                                .borrowerToExpenseMap[participantId]
-                            }
-                          />
-                        </div>
-                      </div>
-                    );
-                  }
-                )}
-              </div>
-
-              <Divider />
-
-              <div className={styles.expenseDivideListItem}>
-                <div className={styles.expenseDivideListItemLeft}>
-                  <strong>Total:</strong>
-                </div>
-
-                <div className={styles.expenseDivideListItemRight}>
-                  <strong>
-                    {getFormattedCurrencyString({
-                      amount:
-                        borrowerExpenseTotal +
-                        newExpenseMeta.distributedTotalExpense
-                          .amountPaidForOwnExpense,
-                    })}
-                  </strong>
-                </div>
-              </div>
-            </>
+            <ExpenseDivideList
+              dispatch={dispatch}
+              newExpenseMeta={newExpenseMeta}
+            />
           ) : (
             <div className={styles.containerBtnStartDividing}>
               <Button
